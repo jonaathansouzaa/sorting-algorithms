31e7eecb2ae6b6361e9c4402a91f4fa64498e01f
Jonathan Souza Santos <jonathan.programmer@gmail.com>
2019-03-27 16:42:48 -0300

    Ajuste do merge sort

---
+++ src/main/java/sort/SortKeys.java
@@ -0,0 +1,8 @@
+package sort;
+
+public class SortKeys {
+
+	public static final String KEY_TROCA = "TROCA";
+	public static final String KEY_COMPARA = "COMPARA";
+	
+}
--- src/test/java/sort/MergeSortTest.java
+++ src/test/java/sort/MergeSortTest.java
@@ -1,16 +1,25 @@
 package sort;
 
-import static org.junit.Assert.assertArrayEquals;
-
 import org.junit.Test;
 
 public class MergeSortTest {
 	
 	@Test
 	public void positiveTest() {
-		int[] actual = { 5, 1, 6, 2, 3, 4 };
-		int[] expected = { 1, 2, 3, 4, 5, 6 };
-		MergeSort.mergeSort(actual, actual.length);
-		assertArrayEquals(expected, actual);
+		int quantidade = 1000;
+		int[] vetor = new int[quantidade];
+		for (int i = 0; i < vetor.length; i++) {
+			vetor[i] = (int) (Math.random() * quantidade);
+		}
+//		int[] vetor = { 10, 20, 15, 7, 1, 25, 9};
+		long tempoInicial = System.currentTimeMillis();
+		MergeSort mergeSort = new MergeSort();
+		mergeSort.mergeSort(vetor, vetor.length);
+		long tempoFinal = System.currentTimeMillis();
+		
+		System.out.println("Executado em = " + (tempoFinal - tempoInicial) + " ms");
+		System.out.println("Comparações: " + mergeSort.getRetornoDados().get(SortKeys.KEY_COMPARA));
+		System.out.println("Troca: " + mergeSort.getRetornoDados().get(SortKeys.KEY_TROCA));
 	}
+	
 }
