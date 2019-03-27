2fa04754d9511a5f1c3010c0aed96b461b7f8640
Jonathan Souza Santos <jonathan.programmer@gmail.com>
2019-03-27 16:43:58 -0300

    Ajuste do ShellSort

---
+++ src/test/java/sort/ShellSortTest.java
@@ -0,0 +1,23 @@
+package sort;
+
+import org.junit.Test;
+
+public class ShellSortTest {
+
+	@Test
+	public void testSomeLibraryMethod() {
+		int quantidade = 10000;
+		int[] vetor = new int[quantidade];
+		for (int i = 0; i < vetor.length; i++) {
+			vetor[i] = (int) (Math.random() * quantidade);
+		}
+		long tempoInicial = System.currentTimeMillis();
+		ShellSort shellSort = new ShellSort();
+		shellSort.shellSort(vetor);
+		long tempoFinal = System.currentTimeMillis();
+		System.out.println("Executado em = " + (tempoFinal - tempoInicial) + " ms");
+		System.out.println("Troca: " + shellSort.getRetornaDados().get(SortKeys.KEY_TROCA));
+		System.out.println("Comparações: " + shellSort.getRetornaDados().get(SortKeys.KEY_COMPARA));
+	}
+	
+}
